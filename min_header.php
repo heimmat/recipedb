4a74758ed7b500255f0e48d3796fe8b9103d500c
heimmat <git.heiming@outlook.com>
2016-09-08 19:13:18 +0200

    Changes to header

---
+++ min_header.php
@@ -0,0 +1,14 @@
+<?php
+	include_once('auth.php');
+?>
+<html>
+<head>
+<meta charset="utf-8"/>
+<title>
+<?php
+	echo $doctitle;
+?>
+</title>
+<link rel="stylesheet" href="recipedb.css">
+</head>
+<body>
